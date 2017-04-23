54aefad6fec8daf5fbf4959f77b151ff425b6799
Alvaro Maceda <alvaro@alvaromaceda.es>
2017-04-23 13:19:08 +0200

    Separados los datos de conexion a BD

---
+++ datosconexion.php.example.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Copia este fichero con el nombre datosconexion.php y los datos de conexión de tu host
+ * Para crear la BD puedes usar el script SQL "SQL para crear la base de datos.sql"
+ */
+$datosconexion = array(
+    "servername" => "", // PON AQUI LA IP DE TU SERVIDOR
+    "username" => "",   // PON AQUI EL USUARIO DE LA BD
+    "password" => "",   // PON AQUI LA CONTRASEÑA DE LA BD
+    "dbname" => "log"
+);
--- randomicon.php
+++ randomicon.php
@@ -1,11 +1,6 @@
 <?php
 
-$datosconexion = array(
-	"servername" => "", 		// PON AQUI LA IP DE TU SERVIDOR
-	"username" => "", 			// PON AQUI EL USUARIO DE LA BD
-	"password" => "", 			// PON AQUI LA CONTRASEÑA DE LA BD
-	"dbname" => "log"
-	);
+require_once 'datosconexion.php';
 
 /*
 
@@ -25,63 +20,71 @@ $icono = chooseRandomPic('icons');
 grabarLog($icono); //Si falla el log no mostraremos el icono
 returnImageData($icono);
 
-function chooseRandomPic($dir = '.'){
+function chooseRandomPic($dir = '.')
+{
     $files = glob($dir . '/*.*');
     $file = array_rand($files);
     return $files[$file];
 }
 
-function setImageHeaders(){
-	header('Content-Type: image/png');
+function setImageHeaders()
+{
+    header('Content-Type: image/png');
 }
 
-function returnImageData($image){
+function returnImageData($image)
+{
     readfile($image);
 }
 
-function grabarLog($nombreicono){
-	global $datosconexion;
-	$conn = conectaBD($datosconexion);
-	$error = insertaRegistro($conn,$nombreicono);
-	if($error!=""){
-		die("No se ha podido insertar en el log: "+$error);
-	}
-	$conn = null;
-	// La conexion se cierra sola cuando la variable conn muere
+function grabarLog($nombreicono)
+{
+    global $datosconexion;
+    $conn = conectaBD($datosconexion);
+    $error = insertaRegistro($conn, $nombreicono);
+    if ($error!="") {
+        die("No se ha podido insertar en el log: "+$error);
+    }
+    $conn = null;
+    // La conexion se cierra sola cuando la variable conn muere
 }
 
-function insertaRegistro($conn,$texto){
-	$sentence = 'INSERT INTO logs (date,logtext) VALUES (:date,:text)';
+function insertaRegistro($conn, $texto)
+{
+    $sentence = 'INSERT INTO logs (date,logtext) VALUES (:date,:text)';
 
-	try{
-		$statement = $conn->prepare($sentence);
-		$statement->bindValue(':date',date("Y-m-d H:i:s"));
-		$statement->bindValue(':text',$texto);
-		$statement->execute();
-	}catch ( PDOException $exception ){
-		return "PDO error :" . $exception->getMessage();
-	}
-	return "";
+    try {
+        $statement = $conn->prepare($sentence);
+        $statement->bindValue(':date', date("Y-m-d H:i:s"));
+        $statement->bindValue(':text', $texto);
+        $statement->execute();
+    } catch (PDOException $exception) {
+        return "PDO error :" . $exception->getMessage();
+    }
+    return "";
 }
 
-function conectaBD($datosconexion){
-	try{
-		$bd = obtenConexionBD($datosconexion);
-	}catch(PDOException $e){
-		die("Connection failed: " . $e->getMessage());
-	}
-	return $bd;
+function conectaBD($datosconexion)
+{
+    try {
+        $bd = obtenConexionBD($datosconexion);
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+    return $bd;
 }
 
-function obtenConexionBD($datosconexion){
+function obtenConexionBD($datosconexion)
+{
     $conn = new PDO(obtenCadenaConexionBD($datosconexion), $datosconexion["username"], $datosconexion["password"]);
     // set the PDO error mode to exception, para poder hacer try/catch
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $conn;   
+    return $conn;
 }
 
-function obtenCadenaConexionBD($datosconexion){
-	return "mysql:host=".$datosconexion["servername"].";dbname=".$datosconexion["dbname"];
+function obtenCadenaConexionBD($datosconexion)
+{
+    return "mysql:host=".$datosconexion["servername"].";dbname=".$datosconexion["dbname"];
 }
 
 ?>
